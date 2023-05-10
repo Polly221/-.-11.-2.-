0cbfb3fac42e1c442bd40c819cfaf9367dbfbd23
Xtrat3 <Xtrat3@XTRAT3>
2023-05-10 22:15:34 +0200

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿try
+{
+    Console.WriteLine("Введите температуру");
+    int temp = int.Parse(Console.ReadLine());
+    { 
+       if(temp>20) Console.WriteLine("Можно выпустить на прогулку слонов, жирафов и львов");
+       else if (temp < 20) Console.WriteLine("Можно выпустить на прогулку только кенгуру");
+            else Console.WriteLine("Температура не входит ни в один из диапазонов");
+    }
+
+}
+catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+}
